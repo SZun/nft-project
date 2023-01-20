3923063c73f877db5d2d171f532160ab05cd8e02
Sam Zun <zunsamuel97@gmail.com>
2023-01-19 21:42:54 -0600

    added second unit test

--- hardhat/test/Spacebear.js
+++ hardhat/test/Spacebear.js
@@ -13,4 +13,17 @@ describe("Spacebear", function () {
 
     expect(await spacebearInstance.ownerOf(0)).to.equal(otherAccount.address);
   });
+
+  it("fails to transfer tokens from the wrong address", async function () {
+    // deploy a lock contract where funds can be withdrawn
+    // one year in the future
+    const Spacebear = await hre.ethers.getContractFactory("Spacebear");
+    const spacebearInstance = await Spacebear.deploy();
+
+    const [owner, nftOwnerAccount, notNftOwnerAccount] = await ethers.getSigners();
+    await spacebearInstance.safeMint(nftOwnerAccount.address)
+
+    expect(await spacebearInstance.ownerOf(0)).to.equal(nftOwnerAccount.address);
+    await expect(spacebearInstance.connect(notNftOwnerAccount).transferFrom(nftOwnerAccount.address, notNftOwnerAccount.address,0)).to.be.revertedWith("ERC721: caller is not token owner nor approved")
+  });
 });
